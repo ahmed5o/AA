3c7b3c5ace3e8f8fabd13400e2e400b1b31d9deb
Ahmed Ayman <your_email@example.com>
2025-09-29 15:11:13 +0300

    Font family done

---
+++ lib/fonts/font_family_helper.dart
@@ -0,0 +1,7 @@
+class FontFamilyHelper {
+  const FontFamilyHelper._();
+  static const String cairoArabic = 'Cairo';
+  static const String poppinsEngligh = 'Poppins';
+
+  //TODO: add switch Arabic font or Engligh font
+}
