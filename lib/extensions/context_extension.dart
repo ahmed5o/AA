f80b0fcad4bd15545c117369514db58a8444e275
Ahmed Ayman <your_email@example.com>
2025-10-01 03:25:28 +0300

    AR & EN done

--- lib/extensions/context_extension.dart
+++ lib/extensions/context_extension.dart
@@ -1,8 +1,13 @@
 import 'package:aa/Theme/color_extension.dart';
+import 'package:aa/core/language/app_localization.dart';
 import 'package:flutter/material.dart';
 
 extension ContextExt on BuildContext {
   MyColor get myColor => Theme.of(this).extension<MyColor>()!;
+
+  String translate(String key) {
+    return AppLocalizations.of(this)!.translate(key) ?? key;
+  }
 
   Future<dynamic> PushName(String routeName, {Object? arguments}) {
     return Navigator.of((this)).pushNamed(
